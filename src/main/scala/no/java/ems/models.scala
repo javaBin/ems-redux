3fb6bcc39574088bf7b4efb650508b0b99682453
Erlend Hamnaberg <erlend@hamnaberg.net>
2011-12-27 11:41:09 +0100

    Make sure that a tag, or keyword cannot contain a comma.

--- src/main/scala/no/java/ems/models.scala
+++ src/main/scala/no/java/ems/models.scala
@@ -260,6 +260,10 @@ case class StreamingAttachment(name: String, size: Option[Long], mediaType: Opti
 
 case class Email(address: String)
 
-case class Tag(name: String)
+case class Tag(name: String) {
+  require(!name.contains(","), "Tag must NOT contain any commas")
+}
 
-case class Keyword(name: String)
+case class Keyword(name: String) {
+  require(!name.contains(","), "Keyword must NOT contain any commas")
+}
