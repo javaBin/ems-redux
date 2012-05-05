7f8803856086feccadd65823109b4cdd5af1713e
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-05-05 14:07:44 +0200

    Added XRD to display the services

--- src/main/scala/no/java/ems/Resources.scala
+++ src/main/scala/no/java/ems/Resources.scala
@@ -4,17 +4,13 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.filter.Plan
 import org.joda.time.DateTime
-import net.hamnaberg.json.collection._
-import no.java.ems.converters._
 import javax.servlet.http.HttpServletRequest
-import no.java.http.URIBuilder
 import no.java.unfiltered._
-import io.Source
-import java.net.URI
 
 trait Resources extends Plan with EventResources with ContactResources with AttachmentHandler { this: Storage =>
 
   def intent = {
+    case req@Path(Seg(Nil)) => handleRoot(req)
     case req@Path(Seg("contacts" :: Nil)) => handleContactList(req)
     case req@Path(Seg("contacts" :: id :: Nil)) => handleContact(id, req)
     case req@Path(Seg("contacts" :: id :: "photo" :: Nil)) => handleContactPhoto(id, req)
@@ -27,6 +23,27 @@ trait Resources extends Plan with EventResources with ContactResources with Atta
     case req@Path(Seg("events" :: eventId :: "sessions" :: sessionId :: "speakers" :: Nil)) => handleSpeakers(eventId, sessionId, req)
     case req@Path(Seg("events" :: eventId :: "sessions" :: sessionId :: "speakers" :: speakerId :: "photo" :: Nil)) => handleSpeakerPhoto(eventId, sessionId, speakerId, req)
     case req@Path(Seg("binary" :: id :: Nil)) => handleAttachment(id, req)
+  }
+
+  def handleRoot(req: HttpRequest[HttpServletRequest]) = {
+    val builder = BaseURIBuilder.getBuilder(req)
+
+    ContentType("application/xrd+xml") ~> ResponseString(
+      <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
+         <Link rel="http://rels.java.no/ems/contacts"
+                href={builder.segments("contacts").toString}>
+           <Title>contacts</Title>
+         </Link>
+         <Link rel="http://rels.java.no/ems/events"
+                href={builder.segments("events").toString}>
+           <Title>events</Title>
+         </Link>
+         <Link rel="http://rels.java.no/ems/binary"
+                href={builder.segments("binary").toString}>
+           <Title>binary</Title>
+         </Link>
+      </XRD>.toString()
+    )
   }
 }
 
