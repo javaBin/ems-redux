341e5f6aa5a9efe30eecc76ba03fc520dd25efd3
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-05-14 20:40:12 +0200

    URIList now works with comments

--- src/main/scala/no/java/ems/URIList.scala
+++ src/main/scala/no/java/ems/URIList.scala
@@ -10,17 +10,15 @@ import io.Source
  * @author Erlend Hamnaberg<erlend@hamnaberg.net>
  */
 case class URIList(list: List[URI]) {
-  def writeTo(writer: Writer) = {
-    list.foreach( u =>
-      writer.write(u.toString + "\r\n")
-    )
+  def writeTo(writer: Writer) {
+    writer.write(list.mkString("", "\r\n", "\r\n"))
   }
 }
 
 object URIList {
   def parse(source: Source): Either[Exception, URIList] = {
     try {
-      Right(URIList(source.getLines().map(line => URI.create(line.trim())).toList))
+      Right(URIList(source.getLines().filterNot(_.startsWith("#")).map(line => URI.create(line.trim())).toList))
     }
     catch {
       case e: Exception => Left(e)
