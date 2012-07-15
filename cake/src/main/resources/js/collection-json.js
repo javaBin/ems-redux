637282ce37a1f6065b207accfce2e7aca0bafbcf
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-07-15 13:52:19 +0200

    Updated collection+json library from trygvis.

--- cake/src/main/resources/js/collection-json.js
+++ cake/src/main/resources/js/collection-json.js
@@ -1,33 +1,61 @@
-//var _ = require('underscore');
+
+function toObject() {
+  return _.reduce(this.data, function(map, field) {
+    map[field.name] = field.value;
+    return map;
+  }, {});
+}
+
+function fixTemplate(t) {
+  t.toObject = toObject;
+  t.data = _.isArray(t.data) ? t.data : [];
+}
 
 /**
- * Takes any object and make it look like a collection+json object with utility methods.
+ * Takes any object and make it look like a collection+json collection object.
  */
 function fromObject(root) {
-  root = _.isObject(root) ? root : {};
-  root.collection = _.isObject(root.collection) ? root.collection : {};
-  var c = root.collection;
-  c.version = _.isString(c.version) ? c.version : "1.0";
+//  console.log('in ', root);
 
-  c.items = _.isArray(c.items) ? c.items : [];
-  _.each(c.items, function(item) {
-    item.links = _.isArray(item.links) ? item.links : [];
-    item.toObject = function() {
-      return _.reduce(item.data, function(map, field) {
-        map[field.name] = field.value;
-        return map;
-      }, {});
+  root.isCollection = function() {
+    return typeof this.collection == 'object';
+  }
+  root.isTemplate = function() {
+    return typeof this.template == 'object';
+  }
+
+  if(root.isTemplate()) {
+    fixTemplate(root.template);
+  }
+  else {
+    root = _.isObject(root) ? root : {collection: {}};
+    if(!_.isObject(root.collection)) {
+      root.collection = {};
     }
-  });
+    var c = root.collection;
 
-  c.links = _.isArray(c.links) ? c.links : [];
+    c.version = _.isString(c.version) ? c.version : "1.0";
+    c.items = _.isArray(c.items) ? c.items : [];
+    _.each(c.items, function(item) {
+      item.links = _.isArray(item.links) ? item.links : [];
+      item.toObject = toObject;
+    });
 
-  c.queries = _.isArray(c.queries) ? c.queries : [];
-  _.each(c.queries, function(query) {
-    query.data = _.isArray(query.data) ? query.data : [];
-  });
+    c.links = _.isArray(c.links) ? c.links : [];
 
-  c.template = _.isObject(c.template) ? c.template : undefined;
+    c.queries = _.isArray(c.queries) ? c.queries : [];
+    _.each(c.queries, function(query) {
+      query.data = _.isArray(query.data) ? query.data : [];
+    });
+
+    if(_.isObject(c.template)) {
+      fixTemplate(c.template);
+    }
+    else {
+      delete c.template;
+    }
+  }
+
   // c.templates = _.isArray(c.templates) ? c.templates : [];
 
   // TODO: make un-enumerable
@@ -41,13 +69,8 @@ function fromObject(root) {
       return rel === link.rel;
     });
   }
-  root.isCollection = function() {
-    return typeof this.collection == 'object';
-  }
-  root.isTemplate = function() {
-    return typeof this.template == 'object';
-  }
 
+//  console.log('out', root);
   return root;
 }
 //module.exports.fromObject = fromObject;
