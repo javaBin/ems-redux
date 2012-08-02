77d3506127dfbf23117f685b4b0ed8a0e112898f
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-08-02 20:03:15 +0200

    Added changelog to root resource. Fixes #5

--- server/src/main/scala/ems/Resources.scala
+++ server/src/main/scala/ems/Resources.scala
@@ -6,7 +6,7 @@ import unfiltered.filter.Plan
 import javax.servlet.http.HttpServletRequest
 import no.java.unfiltered._
 import scala.util.Properties
-import net.hamnaberg.json.collection.{Link, JsonCollection}
+import net.hamnaberg.json.collection.{ValueProperty, Query, Link, JsonCollection}
 
 trait Resources extends Plan with EventResources with ContactResources with AttachmentHandler with ChangelogResources {
 
@@ -37,7 +37,11 @@ trait Resources extends Plan with EventResources with ContactResources with Atta
         Link(builder.segments("contacts").build(), "contact collection"),
         Link(builder.segments("events").build(), "event collection")
       ),
-      Nil
+      Nil,
+      List(Query(builder.segments("changelog").build(), "changelog", Some("Changelog"), List(
+        ValueProperty("type", Some("Type")),
+        ValueProperty("from", Some("From DateTime"))
+      )))
     ))
   }
 }
