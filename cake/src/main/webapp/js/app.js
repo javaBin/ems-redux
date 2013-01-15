d56282eca9b84c967b4f94f427f6862ce43e5cf3
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-01-15 15:32:09 +0100

    We can now load sessions

--- cake/src/main/webapp/js/app.js
+++ cake/src/main/webapp/js/app.js
@@ -5,7 +5,7 @@ angular.module('app', []).
     config(function($routeProvider) {
         $routeProvider.
             when('/', {controller:app.Main, templateUrl: 'about.html'}).
-            when('/events/:eventTitle', {controller:app.SessionList, templateUrl:'sessions.html'})./*.
+            when('/events/:name', {controller:app.SessionList, templateUrl:'sessions.html'})./*.
             when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
             when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).*/
             otherwise({redirectTo:'/'});
@@ -52,13 +52,8 @@ app.LoadEvents = function($scope, $http) {
             var eventHref = root.findLinkByRel("event collection").href;
             console.log(eventHref);
             $http.get(eventHref).success(function(data) {
-                var events = _.map(fromObject(data).collection.items, function(item) {
-                    var i = app.parseItem(item)
-                    i.data.start = app.date.parse(item.data.start);
-                    i.data.end = app.date.parse(item.data.end);
-                    i.sessions = item.findLinkByRel("session collection").href;
-                    return i;
-                });
+                var events = _.map(fromObject(data).collection.items, app.mapEvent);
+                app.events = events;
                 $scope.events = events;
             });
         });
@@ -70,7 +65,31 @@ app.Main = function($scope, $http) {
 }
 
 app.SessionList = function($scope, $routeParams, $http) {
-    console.log("Session list");
-    console.log($routeParams);
-    console.log($scope);
+    var event = _.find(app.events, function(e) {
+        console.log(e.data.name);
+        return e.data.name === $routeParams.name;
+    });
+    if (!event) {
+        app.LoadEvents($scope, $http)
+    }
+    $http.get(event.sessions).success(function(data) {
+        var sessions = _.map(fromObject(data).collection.items, app.mapSession);
+        $scope.sessions = sessions;
+        $scope.name = event.data.name;
+    });
+}
+
+app.mapEvent = function(item) {
+    var i = app.parseItem(item)
+    i.data.start = app.date.parse(item.data.start);
+    i.data.end = app.date.parse(item.data.end);
+    i.sessions = item.findLinkByRel("session collection").href;
+    return i;
+}
+
+app.mapSession = function(item) {
+    var i = app.parseItem(item)
+    console.log("Convert session: " + i.data.title);
+    console.log(i);
+    return i;
 }
