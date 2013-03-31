2b46f7c1cfccdd6764d2873e5d5b3bc95d5b99e3
Anders Karlsen <karlsenanders@hotmail.com>
2013-03-31 09:25:47 +0200

    Dropdown for filtering tags

--- cake/src/main/webapp/js/app.js
+++ cake/src/main/webapp/js/app.js
@@ -113,6 +113,7 @@ app.About = function ($scope) {
 app.SessionList = function ($scope, $routeParams, $http) {
   $scope.numSessions = 0;
   $scope.showingSessions = 0;
+  $scope.usedTags = [];
   app.loadRoot($http, function (root) {
     var query = root.findQueryByRel("event by-slug");
     $http.get(app.wrapAjax(query.expand({"slug": $routeParams.slug})), {cache: true}).success(function (eventCollection) {
@@ -120,6 +121,8 @@ app.SessionList = function ($scope, $routeParams, $http) {
       $http.get(app.wrapAjax(event.sessions)).success(function (data) {
         $scope.sessions = toCollection(data).mapItems(EmsSession);
         $scope.numSessions = $scope.sessions.length;
+        $scope.usedTags = _.map(_.uniq(_.flatten(_.map($scope.sessions,function(session) { return session.object.tags; }))),
+          function(tag) { return {name: tag, selected: false }});
         $scope.showingSessions = $scope.numSessions;
         $scope.filteredSessions = $scope.sessions.slice(0);
         $scope.name = event.object.name;
