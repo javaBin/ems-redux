4de89c25aeb1fc2e450b2d41674107354b1d4e10
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-02-04 21:04:50 +0100

    context path handling.

--- dist/src/main/scala/jetty/Jetty.scala
+++ dist/src/main/scala/jetty/Jetty.scala
@@ -12,17 +12,20 @@ object Jetty extends App {
 
   val server = new Server(Properties.propOrElse("port", "8081").trim.toInt)
 
+  val serverContextPath = Properties.propOrElse("server_contextPath", "/server")
+  val adminContextPath = Properties.propOrElse("admin_contextPath", "/admin")
+
   server.setStopAtShutdown(true)
   server.setSessionIdManager(new HashSessionIdManager())
   server.setHandler({
     val collection = new HandlerCollection()
     collection.addHandler(new WebAppContext(
       new File(home, "webapps/server.war").toString,
-      "/server"
+      serverContextPath
     ))
     collection.addHandler(new WebAppContext(
       new File(home, "webapps/admin.war").toString,
-      "/admin"
+      adminContextPath
     ))
     collection.getHandlers.foreach(h => h.asInstanceOf[WebAppContext].setParentLoaderPriority(true))
     collection
