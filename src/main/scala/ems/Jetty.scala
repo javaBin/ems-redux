415cf4870a6a83543076307da5ba414673b01dcb
Erlend Hamnaberg <erlend@hamnaberg.net>
2014-11-18 14:38:15 +0100

    Context path config

--- src/main/scala/ems/Jetty.scala
+++ src/main/scala/ems/Jetty.scala
@@ -4,8 +4,9 @@ import scala.util.Properties
 
 object Jetty extends App {
   val port = Properties.envOrElse("PORT", "8081").toInt
+  val contextPath = Properties.envOrElse("contextPath", Properties.propOrElse("contextPath", "/server"))
 
-  private val server = unfiltered.jetty.Server.http(port).context("/server") {
+  private val server = unfiltered.jetty.Server.http(port).context(contextPath) {
     _.plan(Resources(ems.security.JAASAuthenticator))
   }
 
