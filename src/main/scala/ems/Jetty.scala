31d284e2efbbca71fcdfa6e49107864dd075414b
Erlend Hamnaberg <erlend@hamnaberg.net>
2015-03-03 23:10:50 +0100

    user app.home

--- src/main/scala/ems/Jetty.scala
+++ src/main/scala/ems/Jetty.scala
@@ -11,8 +11,9 @@ object Jetty extends App {
   val port = Properties.envOrElse("PORT", "8081").toInt
   val contextPath = Properties.propOrElse("contextPath", Properties.envOrElse("contextPath", "/server"))
   val authStrategy = Properties.propOrElse("auth-strategy", Properties.envOrElse("auth-strategy", "jaas"))
+  val home = new File(Properties.propOrElse("app.home", Properties.envOrElse("app.home", ".")))
   val auth = authStrategy match {
-    case "file" => PropertyFileAuthenticator[HttpServletRequest, HttpServletResponse](new File("passwords.properties"))
+    case "file" => PropertyFileAuthenticator[HttpServletRequest, HttpServletResponse](new File(home, "passwords.properties"))
     case _ => JAASAuthenticator
   }
 
