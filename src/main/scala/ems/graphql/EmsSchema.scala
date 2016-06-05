90d518be615c9f01432bcf6d996c70a9e3ebb5d9
Ingar Abrahamsen <ingar@abrahams1.com>
2016-06-06 01:51:37 +0200

    feat: basic graphQl schema
    Listing out events, nothing more.

---
+++ src/main/scala/ems/graphql/EmsSchema.scala
@@ -0,0 +1,53 @@
+package ems.graphql
+
+import java.util.UUID
+
+import ems.model.Event
+import ems.storage.DBStorage
+import sangria.schema._
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+class EmsSchema(store: DBStorage)(implicit executionContext: ExecutionContext) {
+  private val eventType = ObjectType(
+    "Event",
+    fields[Unit, Event](
+      Field("id", StringType, resolve = _.value.id.map(_.toString).getOrElse("")),
+      Field("name", StringType, resolve = _.value.name),
+      Field("venue", StringType, resolve = _.value.venue)
+    ))
+
+  private val argEventId: Argument[Option[Seq[String]]] = Argument(
+    name = "id",
+    argumentType = OptionInputType(ListInputType(IDType)),
+    description = "The event ids"
+  )
+
+  val schema = Schema(
+    ObjectType(
+      "Query",
+      fields[Unit, Unit](
+        Field(
+          "event",
+          ListType(eventType),
+          arguments = argEventId :: Nil,
+          resolve = args => getEvents(args.argOpt[Seq[String]]("id"))
+        )
+      )
+    )
+  )
+
+  def getEvents(ids: Option[Seq[String]]): List[Event] = {
+    val value: Future[List[Event]] = ids.getOrElse(List()) match {
+      case Nil => store.getEvents().map(_.toList)
+      case idsAsString => {
+        val queryIds: Seq[UUID] = idsAsString.map(UUID.fromString)
+        Future.sequence(queryIds.map(store.getEvent).toList)
+            .map(optEvent => optEvent.flatMap(_.toList))
+      }
+    }
+    Await.result(value, 10 seconds)
+  }
+
+}
---
+++ src/test/scala/ems/graphql/EmsSchemaSpec.scala
@@ -0,0 +1,67 @@
+package ems.graphql
+
+import org.json4s.native.JsonMethods._
+import org.specs2.matcher.JsonMatchers
+import org.specs2.mutable.Specification
+import sangria.execution.Executor
+import sangria.marshalling.json4s.native.Json4sNativeResultMarshaller.Node
+import sangria.parser.QueryParser
+
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+import scala.util.Success
+
+class EmsSchemaSpec extends Specification with JsonMatchers {
+
+  import sangria.marshalling.json4s.native._
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  "Schema" should {
+
+    "find all events" in {
+      val node: Node = executeQuery(
+        """
+          | {
+          |   event {
+          |     name
+          |   }
+          | }
+        """.stripMargin)
+
+      pretty(render(node)) must /("data") / ("event") */ ("name" -> "Event 1")
+      pretty(render(node)) must /("data") / ("event") */ ("name" -> "Event 2")
+    }
+
+    "find one event by id" in {
+      val node: Node = executeQuery(
+        """
+          | {
+          |   event(id : "d7af21bd-e040-4e1f-9b45-71918b5e46cd") {
+          |     name
+          |   }
+          | }
+        """.stripMargin)
+
+      pretty(render(node)) must /("data") / ("event") */ ("name" -> "Event 1")
+      pretty(render(node)) must not / ("data") / ("event") */ ("name" -> "Event 2")
+    }
+
+  }
+
+  def executeQuery(query: String): Node = {
+    val schema: EmsSchema = new EmsSchema(new DummyDbStorage)
+    QueryParser.parse(
+      query) match {
+      case Success(dsl) =>
+        Await.result(
+          Executor.execute(
+            schema.schema,
+            dsl
+          ),
+          10 seconds)
+    }
+  }
+
+
+}
