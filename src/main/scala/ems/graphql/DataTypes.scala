a0e9cee4c7947baddb0c5aa7867823315018ed52
Ingar Abrahamsen <ingar@abrahams1.com>
2016-06-08 21:55:51 +0200

    fix: convert uuid and dateTime to string in dataTypes

--- src/main/scala/ems/graphql/DataTypes.scala
+++ src/main/scala/ems/graphql/DataTypes.scala
@@ -2,10 +2,12 @@ package ems.graphql
 
 import java.util.UUID
 
+import ems.util.RFC3339
 import org.joda.time.DateTime
+import sangria.marshalling.MarshallerCapability
 import sangria.schema.ScalarType
 import sangria.validation.ValueCoercionViolation
-import sangria.{ast, schema}
+import sangria.ast
 
 import scala.util.{Failure, Success, Try}
 
@@ -18,7 +20,7 @@ object DataTypes {
   implicit val UUIDType = ScalarType[UUID]("UUID",
     description = Some(
       "The `UUID` scalar type represents a unique identifier"),
-    coerceOutput = schema.valueOutput,
+    coerceOutput = (value: UUID, capabilities: Set[MarshallerCapability]) => value.toString,
     coerceUserInput = {
       case s: String ⇒ Right(UUID.fromString(s))
       case u: UUID ⇒ Right(u)
@@ -33,11 +35,11 @@ object DataTypes {
     description = Some(
       """
         |The `DateTime` scalar type represents the date and time and is based on
-        |the ISO8601 standard. Example of date times: `2010-06-30T01:20+02:00`
+        |the ISO8601/RFC3339 standard. Example of date times: `2010-06-30T01:20+02:00`
         |and `1970-01-01T00:00:00Z`""".stripMargin),
-    coerceOutput = schema.valueOutput,
+    coerceOutput = (value: DateTime, capabilities: Set[MarshallerCapability]) => RFC3339.format(value),
     coerceUserInput = {
-      case s: String ⇒ Right(DateTime.parse(s))
+      case s: String ⇒ RFC3339.parseDateTime(s).fold(l => Left(DateTimeCoercionViolation), r => Right(r))
       case d: DateTime ⇒ Right(d)
       case _ ⇒ Left(DateTimeCoercionViolation)
     },
