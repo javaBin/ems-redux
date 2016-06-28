21138dc1d0a96f264de6676d1337afb9f543cc83
Ingar Abrahamsen <ingar@abrahams1.com>
2016-06-28 22:39:25 +0200

    test: add test for graphQl variables

--- src/test/scala/ems/graphql/EmsSchemaSpec.scala
+++ src/test/scala/ems/graphql/EmsSchemaSpec.scala
@@ -1,6 +1,8 @@
 package ems.graphql
 
 import ems.graphql.DummyDbIds.{eventIdOne, sessionIdOne}
+import org.json4s.JObject
+import org.json4s.JsonAST.{JField, JString}
 import org.json4s.native.JsonMethods._
 import org.specs2.matcher.JsonMatchers
 import org.specs2.mutable.Specification
@@ -62,6 +64,21 @@ class EmsSchemaSpec extends Specification with JsonMatchers {
 
       node must beFailedTry.withThrowable[ValidationError]
     }
+
+    "with variables" in {
+      val node = executeQuery(
+        """
+          | query getEvents($eid: [UUID!]){
+          |   events(id: $eid) {
+          |     name
+          |   }
+          | }
+        """.stripMargin,
+        JObject(List(JField("eid", JString("d7af21bd-e040-4e1f-9b45-71918b5e46cd"))))
+      )
+      pretty(render(node)) must /("data") / ("events") */ ("name" -> "Event 1")
+      pretty(render(node)) must not / ("data") / ("events") */ ("name" -> "Event 2")
+    }
   }
 
   "Schema -> Session" should {
@@ -84,14 +101,15 @@ class EmsSchemaSpec extends Specification with JsonMatchers {
     }
   }
 
-  def executeQuery(query: String): Node = {
+  def executeQuery(query: String, variables: JObject = JObject()): Node = {
     QueryParser.parse(query) match {
       case Success(dsl) =>
         Await.result(
           Executor.execute(
             schema = EmsSchema.EmsSchema,
             queryAst = dsl,
-            userContext = new GraphQlService(new DummyDbStorage)
+            userContext = new GraphQlService(new DummyDbStorage),
+            variables = variables
           ),
           10 seconds)
       case Failure(t) => throw new IllegalStateException(t)
