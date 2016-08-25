67e6ed9e5cb0d799e086b3816481cd18972b0dbb
Erlend Hamnaberg <erlend@hamnaberg.net>
2016-08-25 21:24:08 +0200

    fix build

--- src/test/scala/ems/SessionPermalinksSpec.scala
+++ src/test/scala/ems/SessionPermalinksSpec.scala
@@ -2,14 +2,13 @@ package ems
 
 import org.specs2.mutable.Specification
 import java.net.URI
-import uritemplate._
 
 class SessionPermalinksSpec extends Specification {
   "SessionPermalinks" should {
     "generate correctly in 2015" in {
       val eventId = "0e6d98e9-5b06-42e7-b275-6abadb498c81"
       val expected = URI.create("http://test.2015.javazone.no/details.html?talk=fd09fba268aa5329705298f5f456bb01673edb85ab4a5c6233b3a8d8658ab3cb")
-      val permalinks = SessionPermalinks(Map(eventId -> Expansion("href", URITemplate("http://test.2015.javazone.no/details.html?talk={href}"))))
+      val permalinks = SessionPermalinks(Map(eventId -> Expansion("href", "http://test.2015.javazone.no/details.html?talk={href}")))
       val exp = permalinks.expandHref(eventId, URI.create("http://javazone.no/ems/server/events/0e6d98e9-5b06-42e7-b275-6abadb498c81/sessions/89e097c8-b94e-47fa-9aef-f0b13931cbf5"))
       exp.get must be equalTo(expected)
     }
@@ -17,7 +16,7 @@ class SessionPermalinksSpec extends Specification {
       val eventId = "0e6d98e9-5b06-42e7-b275-6abadb498c81"
       val expected = URI.create("https://2016.javazone.no/program/hjelp-vi-skal-kode-funksjonelt-i-java")
       val expected2 = URI.create("https://2016.javazone.no/program/enterprise-programvare-vart-du-skraemt-no")
-      val permalinks = SessionPermalinks(Map(eventId -> Expansion("title", URITemplate("https://2016.javazone.no/program/{title}"))))
+      val permalinks = SessionPermalinks(Map(eventId -> Expansion("title", "https://2016.javazone.no/program/{title}")))
       val exp = permalinks.expandTitle(eventId, "Hjelp, vi skal kode funksjonelt i Java!")
       val exp2 = permalinks.expandTitle(eventId, "Enterprise programvare!   Vart du skræmt no?")
       exp.get must be equalTo(expected)
